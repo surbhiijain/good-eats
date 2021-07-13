1a279bc18ff25ce6e457cb5dc037a78b941e0e6e
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-12 18:04:15 -0700

    created tab bar controller with initial tabs

---
+++ GoodEats/GoodEats/View Controllers/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MapViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GoodEats/GoodEats/View Controllers/MapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewController.m
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/12/21.
+//
+
+#import "MapViewController.h"
+#import <MapKit/MapKit.h>
+
+@interface MapViewController ()
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // setting initial visible region of map to SF
+    // todo: change to user's current location
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.mapView setRegion:sfRegion animated:false];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
