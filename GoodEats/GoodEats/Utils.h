05334fcc7533590f0bd5785cbe6851ae3b54b561
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-21 12:29:36 -0700

    refactored view controllers to use setStarFills from Utils file

---
+++ GoodEats/GoodEats/Utils.h
@@ -0,0 +1,12 @@
+//
+//  Utils.h
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/21/21.
+//
+
+#ifndef Utils_h
+#define Utils_h
+
+
+#endif /* Utils_h */
---
+++ GoodEats/GoodEats/Utils.m
@@ -0,0 +1,8 @@
+//
+//  Utils.m
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/21/21.
+//
+
+#import <Foundation/Foundation.h>
