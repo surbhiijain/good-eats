537270f76863db051daf92c048b66188b75b5a70
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-21 15:16:39 -0700

    [Restaurant Details][6/x] created view controller files for feed views

---
+++ GoodEats/GoodEats/View Controllers/RestaurantCollectionFeedViewController.h
@@ -0,0 +1,16 @@
+//
+//  RestaurantCollectionFeedViewController.h
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/21/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RestaurantCollectionFeedViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GoodEats/GoodEats/View Controllers/RestaurantTableViewFeedViewController.h
@@ -0,0 +1,16 @@
+//
+//  RestaurantTableViewFeedViewController.h
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/21/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RestaurantTableViewFeedViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
