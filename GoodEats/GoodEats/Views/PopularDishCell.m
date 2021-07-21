bbdf43351741df47ba570195a3c60c9221667a0b
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-21 15:08:20 -0700

    [Restaurant Details][5/x] add image to popular dish cell

--- GoodEats/GoodEats/Views/PopularDishCell.m
+++ GoodEats/GoodEats/Views/PopularDishCell.m
@@ -7,6 +7,7 @@
 
 #import "PopularDishCell.h"
 #import "Utils.h"
+#import "Post.h"
 
 @implementation PopularDishCell
 
@@ -23,6 +24,7 @@
     self.dishNameLabel.text = self.dish.name;
     self.numCheckInsLabel.text = [NSString stringWithFormat:@"%@ check ins", self.dish.numCheckIns];
     [self setStars];
+    [self setDishImageView];
 }
 
 - (void)prepareForReuse {
@@ -46,6 +48,29 @@
     [stars addObject:self.reviewStar5];
 
     [Utils setStarFills:self.dish.avgRating withStars:stars];
+}
+
+- (void) setDishImageView {
+    [self getDishImageWithCompletion:^(NSArray *posts, NSError *error) {
+        if (posts) {
+            Post *post = posts[0];
+            [post.image getDataInBackgroundWithBlock:^(NSData *imageData, NSError *error) {
+                UIImage *image = [UIImage imageWithData:imageData];
+                [self.dishImageView setImage:image] ;
+            }];
+        }
+    }];
+}
+
+- (void)getDishImageWithCompletion:(void(^)(NSArray *posts, NSError *error))completion {
+    PFQuery *query = [PFQuery queryWithClassName:@"Post"];
+    [query orderByDescending:@"createdAt"];
+    query.limit = 1;
+    [query includeKey:@"image"];
+    [query whereKey:@"dish" equalTo:self.dish];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        completion(objects, error);
+    }];
 }
 
 @end
