cb2e6232976cdd1cb355cfe0521a3c5b988ef457
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-22 18:26:45 -0700

    [Filters][1/x] created and connected view controller to new files

---
+++ GoodEats/GoodEats/View Controllers/FilterViewController.h
@@ -0,0 +1,16 @@
+//
+//  FilterViewController.h
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FilterViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GoodEats/GoodEats/View Controllers/FilterViewController.m
@@ -0,0 +1,38 @@
+//
+//  FilterViewController.m
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/22/21.
+//
+
+#import "FilterViewController.h"
+
+@interface FilterViewController ()
+@property (weak, nonatomic) IBOutlet UISegmentedControl *distanceSegControl;
+
+@property (weak, nonatomic) IBOutlet UIButton *tagButton1;
+@property (weak, nonatomic) IBOutlet UIButton *tagButton2;
+@property (weak, nonatomic) IBOutlet UIButton *tagButton3;
+@property (weak, nonatomic) IBOutlet UIButton *tagButton4;
+@property (weak, nonatomic) IBOutlet UIButton *tagButton5;
+
+@end
+
+@implementation FilterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
