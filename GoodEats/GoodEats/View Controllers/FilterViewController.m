8d4e65f5d36615758ebd3dc5e86d54f68b8b898e
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-22 19:12:21 -0700

    [Filters 3/x] created method to filter by distance

--- GoodEats/GoodEats/View Controllers/FilterViewController.m
+++ GoodEats/GoodEats/View Controllers/FilterViewController.m
@@ -6,8 +6,11 @@
 //
 
 #import "FilterViewController.h"
+#import <Parse/Parse.h>
+#import "Restaurant.h"
+#import "LocationManager.h"
 
-@interface FilterViewController ()
+@interface FilterViewController () <LocationManagerDelegate>
 @property (weak, nonatomic) IBOutlet UISegmentedControl *distanceSegControl;
 
 @property (weak, nonatomic) IBOutlet UIButton *tagButton1;
@@ -16,13 +19,80 @@
 @property (weak, nonatomic) IBOutlet UIButton *tagButton4;
 @property (weak, nonatomic) IBOutlet UIButton *tagButton5;
 
+@property (nonatomic, strong) NSMutableArray *restaurants;
+@property (nonatomic, strong) NSMutableArray *filteredRestaurants;
+
+
+@property (nonatomic, strong) LocationManager *locationManager;
+@property (nonatomic, strong) CLLocation *userLocation;
+
 @end
 
 @implementation FilterViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.locationManager = [[LocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self.locationManager setUpLocationManager];
+    
+    self.filteredRestaurants = [[NSMutableArray alloc] init];
+    
+}
+- (IBAction)didApplyFilters:(UIButton *)sender {
+    [self getAllRestaurants];
+
+}
+
+- (double) getFilteredDistance {
+    switch (self.distanceSegControl.selectedSegmentIndex) {
+        case 0:
+            return 1;
+        case 1:
+            return 5;
+        case 2:
+            return 20;
+        default:
+            break;
+    }
+    return 0;
+}
+
+- (void) getAllRestaurants {
+    PFQuery *query = [PFQuery queryWithClassName:@"Restaurant"];
+    [query includeKeys:@[@"dishes"]];
+    [query orderByDescending:@"createdAt"];
+    query.limit = 25;
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *restaurants, NSError *error) {
+        if (restaurants != nil) {
+            self.restaurants = (NSMutableArray *) restaurants;
+            double filterDistance = [self getFilteredDistance];
+            [self filterRestaurantsByDistance:filterDistance fromLocation:self.userLocation];
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+}
+
+- (void)LocationManager:(LocationManager *)locationManager setUpWithLocation:(CLLocation *)location {
+    self.userLocation = location;
+}
+
+- (void) filterRestaurantsByDistance: (double) filterDistance fromLocation: (CLLocation *) userLocation {
+    NSMutableArray *distances = [[NSMutableArray alloc] init];
+    for (Restaurant *restaurant in self.restaurants) {
+        CLLocation *restaurantLocation = [[CLLocation alloc] initWithLatitude:[restaurant.latitude doubleValue] longitude:[restaurant.longitude doubleValue]];
+        
+        CLLocationDistance locationDistance = [restaurantLocation distanceFromLocation:userLocation];
+        double distanceInMiles = locationDistance * 0.000621371;
+
+        
+        if (distanceInMiles <= filterDistance) {
+            [self.filteredRestaurants addObject:restaurant];
+        }
+    }
 }
 
 /*
