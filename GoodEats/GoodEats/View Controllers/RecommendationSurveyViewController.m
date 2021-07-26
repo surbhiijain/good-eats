d03afc642c8e1e6bacdaba83b548aba90c697dcc
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-26 11:32:11 -0700

    [Recommendation][2/x] get and store survey results

--- GoodEats/GoodEats/View Controllers/RecommendationSurveyViewController.m
+++ GoodEats/GoodEats/View Controllers/RecommendationSurveyViewController.m
@@ -6,11 +6,17 @@
 //
 
 #import "RecommendationSurveyViewController.h"
+#import "DishDetailsViewController.h"
+#import "Dish.h"
+#import "Restaurant.h"
+#import "Post.h"
 
 @interface RecommendationSurveyViewController ()
 @property (weak, nonatomic) IBOutlet UISegmentedControl *timeSegControl;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tasteSegControl;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *adventureSegControl;
+
+@property (nonatomic, strong) Dish *dish;
 
 @end
 
@@ -18,10 +24,61 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 - (IBAction)didTapDone:(id)sender {
+    double dist = [self getSelectedDistance];
+    BOOL taste = [self getTasteImportanceBool];
+    BOOL new = [self getTrySomethingNewBool];
 }
 
+- (double) getSelectedDistance {
+    switch (self.timeSegControl.selectedSegmentIndex) {
+        case 0:
+            return 1;
+        case 1:
+            return 5;
+        case 2:
+            return 15;
+        case 3:
+            return 30;
+        default:
+            break;
+    }
+    return 0;
+}
+
+- (BOOL) getTasteImportanceBool {
+    switch (self.tasteSegControl.selectedSegmentIndex) {
+        case 0:
+            return TRUE;
+        case 1:
+            return FALSE;
+        default:
+            break;
+    }
+    return TRUE;
+}
+
+- (BOOL) getTrySomethingNewBool {
+    switch (self.adventureSegControl.selectedSegmentIndex) {
+        case 0:
+            return FALSE;
+        case 1:
+            return TRUE;
+        default:
+            break;
+    }
+    return FALSE;
+}
+
+
+#pragma mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"dishDetailsSegue"]) {
+        DishDetailsViewController *detailsVC = [segue destinationViewController];
+        detailsVC.dish = self.dish;
+    }
+}
 
 @end
