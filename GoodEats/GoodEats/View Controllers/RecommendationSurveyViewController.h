cc8a5af6317c0a52e34cd3757856bc358103c03c
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-26 09:50:14 -0700

    [Recommendation][1/x] created survey view controller

---
+++ GoodEats/GoodEats/View Controllers/RecommendationSurveyViewController.h
@@ -0,0 +1,16 @@
+//
+//  RecommendationSurveyViewController.h
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/26/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RecommendationSurveyViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GoodEats/GoodEats/View Controllers/RecommendationSurveyViewController.m
@@ -0,0 +1,27 @@
+//
+//  RecommendationSurveyViewController.m
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/26/21.
+//
+
+#import "RecommendationSurveyViewController.h"
+
+@interface RecommendationSurveyViewController ()
+@property (weak, nonatomic) IBOutlet UISegmentedControl *timeSegControl;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tasteSegControl;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *adventureSegControl;
+
+@end
+
+@implementation RecommendationSurveyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)didTapDone:(id)sender {
+}
+
+
+@end
