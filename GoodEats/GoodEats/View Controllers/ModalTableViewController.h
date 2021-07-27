d476c2e5aaf03e233d56086e04fc6e557bc08e91
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-07-27 09:50:01 -0700

    [Modal Card UI][1/x] setup modal card xib file

---
+++ GoodEats/GoodEats/View Controllers/ModalTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  ModalTableViewController.h
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/26/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ModalTableViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GoodEats/GoodEats/View Controllers/ModalTableViewController.m
@@ -0,0 +1,31 @@
+//
+//  ModalTableViewController.m
+//  GoodEats
+//
+//  Created by Surbhi Jain on 7/26/21.
+//
+
+#import "ModalTableViewController.h"
+
+@interface ModalTableViewController ()
+
+@end
+
+@implementation ModalTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
