a8ab2c06cddcfffa2cc57e669878d0e487935678
Surbhi Jain <30298846+surbhiijain@users.noreply.github.com>
2021-08-03 10:36:19 -0700

    [Profile Pictures] added ability to set and display user profile pic

--- GoodEats/GoodEats/View Controllers/ProfileViewController.m
+++ GoodEats/GoodEats/View Controllers/ProfileViewController.m
@@ -20,6 +20,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *numCheckInsLabel;
 
+@property (weak, nonatomic) IBOutlet UIButton *profileImageButton;
+
 @property (weak, nonatomic) IBOutlet UIButton *mostRecentGoodEatButton1;
 @property (weak, nonatomic) IBOutlet UIButton *mostRecentGoodEatButton2;
 @property (weak, nonatomic) IBOutlet UIButton *mostRecentGoodEatButton3;
@@ -30,7 +32,6 @@
 @property (nonatomic, strong) NSArray *topThreeRecentPosts;
 
 @property (nonatomic, strong) Post *selectedPost;
-
 
 @property (nonatomic, strong) PFUser *user;
 
@@ -43,6 +44,7 @@
     [super viewDidLoad];
     
     self.user = [PFUser currentUser];
+    [self setProfilePhoto];
     
     self.usernameLabel.text = self.user.username;
     
@@ -61,6 +63,92 @@
     CGFloat itemWidth = (self.collectionView.frame.size.width - layout.minimumInteritemSpacing * (postsPerLine + 1)) / postsPerLine;
     CGFloat itemHeight = itemWidth * 3 / 4;
     layout.itemSize = CGSizeMake(itemWidth, itemHeight);
+}
+
+- (void)setProfilePhoto {
+    PFFileObject *temp_file = self.user[@"image"];
+    if (temp_file) {
+        [temp_file getDataInBackgroundWithBlock:^(NSData *imageData, NSError *error) {
+                UIImage *image = [UIImage imageWithData:imageData];
+            [self.profileImageButton setImage:image forState:UIControlStateNormal];
+            }];
+    } else {
+        UIImage *image = [UIImage systemImageNamed:@"person.circle.filled"];
+        [self.profileImageButton setImage:image forState:UIControlStateNormal];
+    }
+
+    self.profileImageButton.layer.cornerRadius = 50;
+    self.profileImageButton.layer.masksToBounds = true;
+}
+
+- (IBAction)didTapProfilePhoto:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    
+    // create menu options for users to either take a photo with the camera or upload from their photo library if their device supports a camera
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        NSMutableArray *photoOptions = [NSMutableArray new];
+        
+        UIAction *camera = [UIAction actionWithTitle:@"Camera" image:nil identifier:nil handler:^(UIAction* action) {
+            imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+            [self presentViewController:imagePickerVC animated:YES completion:nil];
+        }];
+        [photoOptions addObject:camera];
+        
+        
+        UIAction *photoLibrary = [UIAction actionWithTitle:@"Photo Library" image:nil identifier:nil handler:^(UIAction* action) {
+            imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+            [self presentViewController:imagePickerVC animated:YES completion:nil];
+        }];
+        [photoOptions addObject:photoLibrary];
+        
+        [self.profileImageButton setMenu:[UIMenu menuWithChildren:photoOptions]];
+    }
+    // otherwise, go directly into the photo library
+    else {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        [self presentViewController:imagePickerVC animated:YES completion:nil];
+    }
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    UIImage *resizedImage = [self resizeImage:editedImage withSize:CGSizeMake(500, 500)];
+    
+    [self setUserProfileImageWithImage:resizedImage];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image
+                withSize:(CGSize)size {
+    
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (void) setUserProfileImageWithImage:(UIImage *)image {
+        
+    NSData *imageData = UIImagePNGRepresentation(image);
+    PFFileObject *imageObject = [PFFileObject fileObjectWithName:@"name.png" data:imageData];
+    self.user[@"image"] = imageObject;
+    
+    [[PFUser currentUser] saveInBackground];
+
+    [self.profileImageButton setBackgroundImage:nil forState:UIControlStateNormal];
+    [self.profileImageButton setImage:image forState:UIControlStateNormal];
 }
 
 - (void) fetchAllPosts {
